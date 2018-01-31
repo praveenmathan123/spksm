d5efed7d5c587508570ce0980b427ae0f4761f6b
praveenmathan123 <30828304+praveenmathan123@users.noreply.github.com>
2018-01-31 16:12:40 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,count=0;
+    int str[i];
+    printf("enter the string");
+    for(i=0;a[i]!=null;i++)
+    {
+        if((a[i]>='0'&&a[i]<='9'));
+        count++;
+    }
+}
+return0;
+}
